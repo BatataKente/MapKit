8c21b9d069dd99de787620373cfbf8ec2cc68903
Daniel <daniel.oliveira@foursys.com.br>
2022-10-11 13:49:05 -0300

    Updated info and market

--- MapKit_2/View.swift
+++ MapKit_2/View.swift
@@ -5,11 +5,14 @@
 //  Created by Josicleison on 11/10/22.
 //
 
+import CoreLocation
 import MapKit
 import UIKit
 
 class View: UIViewController {
 
+    private let locationManager = CLLocationManager()
+    
     private let mapView: MKMapView = {
 
         let mapView = MKMapView()
@@ -27,5 +30,39 @@ class View: UIViewController {
 
         mapView.constraint(by: [.top,.leading,.trailing,.bottom])
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    
+    private func render(_ location: CLLocation) {
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
+                                            longitude: location.coordinate.longitude)
+        
+        mapView.setRegion(MKCoordinateRegion(center: center,
+                                             span: MKCoordinateSpan(latitudeDelta: 0.1,
+                                                                    longitudeDelta: 0.1)),
+                          animated: true)
+    }
 }
 
+extension View: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        
+        if let location = locations.first {
+            
+            locationManager.stopUpdatingLocation()
+            
+            render(location)
+        }
+    }
+}
